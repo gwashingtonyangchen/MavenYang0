25b88f0b99e0a46598a61b15c0caa0ad7ee9429b
gwashingtonyangchen <gwashingtonyangchen@yahoo.com>
2021-05-12 13:35:54 -0500

    update

--- MavenBeans/src/main/java/bean/bean2.java
+++ MavenBeans/src/main/java/bean/bean2.java
@@ -1,13 +1,20 @@
 package bean;
 
 import bean.driver;
-
 public class bean2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		driver driver1 = new driver();
 		driver driver2 = new driver();
+		driver1.setAge("Red");
+		driver1.setName("Yang");
+		driver1.setWeight(200);
+		driver2.setAge("Green");
+		driver2.setName("George");
+		driver2.setWeight(300);
+		System.out.println(driver1.toString());
+		System.out.println(driver2.toString());
 		System.out.println("My instructor is awesome!");
 	}
 
--- MavenBeans/src/main/java/com/Maven1/beans/bean1.java
+++ MavenBeans/src/main/java/com/Maven1/beans/bean1.java
@@ -6,6 +6,14 @@ public class bean1 {
 		// TODO Auto-generated method stub
        driver driver1 =new driver();
        driver driver2 =new driver();
+       driver1.setAge(25);
+		driver1.setName("Yang");
+		driver1.setWeight(200);
+		driver2.setAge(30);
+		driver2.setName("George");
+		driver2.setWeight(300);
+		System.out.println(driver1.toString());
+		System.out.println(driver2.toString());
        System.out.println("Matt is awesome!");
 	}
 
